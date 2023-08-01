6e97548abd547d680e4ae123628d1625c039d453
Mihirpalkhiwala2000 <palkhiwalamihir@gmail.com>
2023-08-01 15:56:55 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const multer = require("multer");
+const express = require("express");
+const xlsx = require("xlsx");
+
+const app = express();
+const PORT = 3000;
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    return cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    return cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+
+app.get("/", (req, res) => {
+  return res.render("homepage");
+});
+
+app.post("/merge", upload.array("files", 5), async (req, res) => {
+  try {
+    const mergedWorkbook = xlsx.utils.book_new();
+    if (req.files.length === 1) {
+      throw Error("Only 1 file provided, please provide more files");
+    }
+    for (let i = 0; i < req.files.length; i++) {
+      const workbook = xlsx.readFile(req.files[i].path);
+      xlsx.utils.book_append_sheet(
+        mergedWorkbook,
+        workbook.Sheets[workbook.SheetNames[0]],
+        `Sheet${i + 1}`
+      );
+    }
+
+    const mergedBuffer = xlsx.write(mergedWorkbook, { type: "buffer" });
+
+    res.setHeader("Content-Disposition", 'attachment; filename="merged.xlsx');
+    res.send(mergedBuffer);
+  } catch (e) {
+    res.send(e.message);
+  }
+});
+
+app.listen(PORT, () => console.log("Server started on port: 3000"));
